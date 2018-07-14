fbc126b44458281a59a751350bf96e2b16a19f7c
pnbao <pnbao96@gmail.com>
2018-07-15 01:27:32 +0700

    Update json structure

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    if req.get("result").get("action") == "Feedback":
+    if req.get("queryResult").get("action") == "Feedback":
         baseurl = "https://hooks.zapier.com/hooks/catch/2399943/wpr974/"
         r = requests.post(baseurl, data=json.dumps(req))
-    elif req.get("result").get("action") == "RegisterOnline":
+    elif req.get("queryResult").get("action") == "RegisterOnline":
         baseurl = "https://hooks.zapier.com/hooks/catch/2399943/wp99ov/"
         r = requests.post(baseurl, data=json.dumps(req))
     else:
         return "Nothing"
-    return r.text
+    return r.request.body
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='localhost')
