9472ec238fc7b673f964f74671ec190fda7e2721
pnbao <pnbao96@gmail.com>
2018-07-17 23:52:34 +0700

    Update response to JSON format

--- app.py
+++ app.py
@@ -12,15 +12,60 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
+    message = req.get("result").get("fulfillment").get("messages")[0]["speech"]
+    responseMessage = json.dumps({
+        "speech": message,
+        "displayText": message,
+        "messages": {
+            "type": 1,
+            "title": "card title",
+            "subtitle": "card text",
+            "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
+        },
+        "data": {
+            "google": {
+                "expectUserResponse": True,
+                "richResponse": {
+                    "items": [
+                        {
+                            "simpleResponse": {
+                                "textToSpeech": message
+                            }
+                        }
+                    ]
+                }
+            },
+            "facebook": {
+                "text": message
+            },
+            "slack": {
+                "text": message
+            }
+        },
+        "contextOut": [
+            {
+                "name": "context name",
+                "lifespan": 5,
+                "parameters": {
+                    "param": "param value"
+                }
+            }
+        ],
+        "source": "example.com",
+        "followupEvent": {
+            "name": "event name",
+            "parameters": {
+                "param": "param value"
+            }
+        }
+    })
     if req.get("result").get("action") == "Feedback":
         baseurl = "https://hooks.zapier.com/hooks/catch/2399943/wpr974/"
         r = requests.post(baseurl, data=json.dumps(req))
     elif req.get("result").get("action") == "RegisterOnline":
         baseurl = "https://hooks.zapier.com/hooks/catch/2399943/wp99ov/"
         r = requests.post(baseurl, data=json.dumps(req))
-    else:
-        return "Nothing"
-    return r.request.body
+    return responseMessage
 
 
 if __name__ == '__main__':
